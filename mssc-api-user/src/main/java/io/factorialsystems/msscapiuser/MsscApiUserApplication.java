788aa5ed2caac34ec6bff3f7c38805a073ca7bb1
Adebola Omoboya <adeomoboya@gmail.com>
2022-03-05 19:30:39 +0100

    API Users 1st Commit

---
+++ mssc-api-user/src/main/java/io/factorialsystems/msscapiuser/MsscApiUserApplication.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.msscapiuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscApiUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscApiUserApplication.class, args);
+    }
+
+}
---
+++ mssc-api-user/src/test/java/io/factorialsystems/msscapiuser/MsscApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.msscapiuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscApiUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
