fc20556e70bb538ce1933b7a0ab25f71cc53d8b6
Adebola Omoboya <adeomoboya@gmail.com>
2023-03-11 12:52:46 +0100

    Changes for migration from Keycloak 15 to 20

--- keycloak-events/src/main/java/io/factorialsystems/keycloakevents/listener/OnecardEventListenerProvider.java
+++ keycloak-events/src/main/java/io/factorialsystems/keycloakevents/listener/OnecardEventListenerProvider.java
@@ -3,6 +3,7 @@ package io.factorialsystems.keycloakevents.listener;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.factorialsystems.keycloakevents.utils.User;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.jms.pool.PooledConnectionFactory;
 import org.jboss.logging.Logger;
 import org.keycloak.events.Event;
@@ -19,12 +20,10 @@ import javax.jms.*;
 public class OnecardEventListenerProvider implements EventListenerProvider {
     private final RealmProvider model;
     private final KeycloakSession session;
-    private final PooledConnectionFactory pooledConnectionFactory;
     private static final Logger LOG = Logger.getLogger(OnecardEventListenerProvider.class);
 
-    public OnecardEventListenerProvider(KeycloakSession session, PooledConnectionFactory pooledConnectionFactory) {
+    public OnecardEventListenerProvider(KeycloakSession session) {
         this.session = session;
-        this.pooledConnectionFactory = pooledConnectionFactory;
         this.model = session.realms();
     }
 
@@ -33,49 +32,55 @@ public class OnecardEventListenerProvider implements EventListenerProvider {
 
         LOG.infov("Event Type {0} Captured", event.getType().toString());
 
-        if (EventType.REGISTER.equals(event.getType())) {
-            String id = event.getUserId();
-            RealmModel realmModel = model.getRealm(event.getRealmId());
-            UserModel userModel = session.users().getUserById(id, realmModel);
+       if (EventType.REGISTER.equals(event.getType()) || EventType.LOGIN.equals(event.getType())) {
+           final ActiveMQConnectionFactory connectionFactory = createActiveMQConnectionFactory();
+           final PooledConnectionFactory pooledConnectionFactory = createPooledConnectionFactory(connectionFactory);
 
-//            final ActiveMQConnectionFactory connectionFactory = createActiveMQConnectionFactory();
-//            final PooledConnectionFactory pooledConnectionFactory = createPooledConnectionFactory(connectionFactory);
+           if (EventType.REGISTER.equals(event.getType())) {
+               String id = event.getUserId();
+               RealmModel realmModel = model.getRealm(event.getRealmId());
+               UserModel userModel = session.users().getUserById(realmModel, id);
 
-            User user = User.builder()
-                    .id(id)
-                    .username(userModel.getUsername())
-                    .firstName(userModel.getFirstName())
-                    .lastName(userModel.getLastName())
-                    .email(userModel.getEmail())
-                    .emailVerified(userModel.isEmailVerified())
-                    .enabled(userModel.isEnabled())
-                    .build();
+               User user = User.builder()
+                       .id(id)
+                       .username(userModel.getUsername())
+                       .firstName(userModel.getFirstName())
+                       .lastName(userModel.getLastName())
+                       .email(userModel.getEmail())
+                       .emailVerified(userModel.isEmailVerified())
+                       .enabled(userModel.isEnabled())
+                       .build();
 
-            LOG.info(String.format("Sending Registration Message for User %s", user.getUsername()));
+               LOG.info(String.format("Sending Registration Message for User %s", user.getUsername()));
 
-            try {
-                sendMessage(user, "UserQueue");
-            } catch (JMSException ex) {
-                LOG.error(String.format("JMSException : %s", ex.getMessage()));
-                ex.printStackTrace();
-            } catch (JsonProcessingException e) {
-                LOG.error(String.format("JsonProcessingException : %s", e.getMessage()));
-                e.printStackTrace();
-            }
-        } else if (EventType.LOGIN.equals(event.getType())) {
-            String id = event.getUserId();
-            LOG.info(String.format("Sending Login message for %s", id));
+               try {
+                   sendMessage(user, "UserQueue", pooledConnectionFactory);
+               } catch (JMSException ex) {
+                   LOG.error(String.format("JMSException : %s", ex.getMessage()));
+                   ex.printStackTrace();
+               } catch (JsonProcessingException e) {
+                   LOG.error(String.format("JsonProcessingException : %s", e.getMessage()));
+                   e.printStackTrace();
+               } finally {
+                   pooledConnectionFactory.stop();
+               }
+           } else if (EventType.LOGIN.equals(event.getType())) {
+               String id = event.getUserId();
+               LOG.info(String.format("Sending Login message for %s", id));
 
-            try {
-                sendMessage(id, "LoginQueue");
-            } catch (JMSException ex) {
-                LOG.error(String.format("JMSException : %s", ex.getMessage()));
-                ex.printStackTrace();
-            } catch (JsonProcessingException e) {
-                LOG.error(String.format("JsonProcessingException : %s", e.getMessage()));
-                e.printStackTrace();
-            }
-        }
+               try {
+                   sendMessage(id, "LoginQueue", pooledConnectionFactory);
+               } catch (JMSException ex) {
+                   LOG.error(String.format("JMSException : %s", ex.getMessage()));
+                   ex.printStackTrace();
+               } catch (JsonProcessingException e) {
+                   LOG.error(String.format("JsonProcessingException : %s", e.getMessage()));
+                   e.printStackTrace();
+               } finally {
+                   pooledConnectionFactory.stop();
+               }
+           }
+       }
     }
 
     @Override
@@ -86,7 +91,31 @@ public class OnecardEventListenerProvider implements EventListenerProvider {
     public void close() {
     }
 
-    private void sendMessage(Object object, String queue) throws JMSException, JsonProcessingException {
+        private ActiveMQConnectionFactory createActiveMQConnectionFactory() {
+        String jmsUrl = System.getenv("JMS_URL");
+        String jmsUser = System.getenv("JMS_USER");
+        String jmsPassword = System.getenv("JMS_PASSWORD");
+
+        LOG.info(String.format("URL: %s, User: %s, Password: %s", jmsUrl, jmsUser, jmsPassword));
+
+        // Create a connection factory.
+        final ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(jmsUrl);
+
+        // Pass the username and password.
+        connectionFactory.setUserName(jmsUser);
+        connectionFactory.setPassword(jmsPassword);
+        return connectionFactory;
+    }
+
+    private PooledConnectionFactory createPooledConnectionFactory(ActiveMQConnectionFactory connectionFactory) {
+        // Create a pooled connection factory.
+        final PooledConnectionFactory pooledConnectionFactory = new PooledConnectionFactory();
+        pooledConnectionFactory.setConnectionFactory(connectionFactory);
+        pooledConnectionFactory.setMaxConnections(2);
+        return pooledConnectionFactory;
+    }
+
+    private void sendMessage(Object object, String queue, PooledConnectionFactory pooledConnectionFactory) throws JMSException, JsonProcessingException {
         // Establish a connection for the producer.
         final Connection producerConnection = pooledConnectionFactory.createConnection();
         producerConnection.start();
--- keycloak-events/src/main/java/io/factorialsystems/keycloakevents/listener/OnecardEventListenerProviderFactory.java
+++ keycloak-events/src/main/java/io/factorialsystems/keycloakevents/listener/OnecardEventListenerProviderFactory.java
@@ -1,8 +1,6 @@
 package io.factorialsystems.keycloakevents.listener;
 
 import io.factorialsystems.keycloakevents.utils.Utils;
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.jms.pool.PooledConnectionFactory;
 import org.jboss.logging.Logger;
 import org.keycloak.Config;
 import org.keycloak.events.EventListenerProvider;
@@ -11,20 +9,13 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 
 public class OnecardEventListenerProviderFactory implements EventListenerProviderFactory {
-    private String jmsUrl;
-    private String jmsUser;
-    private String jmsPassword;
 
     private static final String ID = "onecard-event-listener";
     private static final Logger LOG = Logger.getLogger(OnecardEventListenerProviderFactory.class);
 
     @Override
     public EventListenerProvider create(KeycloakSession keycloakSession) {
-
-        final ActiveMQConnectionFactory connectionFactory = createActiveMQConnectionFactory();
-        final PooledConnectionFactory pooledConnectionFactory = createPooledConnectionFactory(connectionFactory);
-
-        OnecardEventListenerProvider provider = new OnecardEventListenerProvider(keycloakSession, pooledConnectionFactory);
+        OnecardEventListenerProvider provider = new OnecardEventListenerProvider(keycloakSession);
         keycloakSession.enlistForClose(provider);
 
         return provider;
@@ -34,39 +25,15 @@ public class OnecardEventListenerProviderFactory implements EventListenerProvide
     public void init(Config.Scope scope) {
         LOG.infov("Init Keycloak extension component={0}", Utils.toComponentIdString(this));
 
-        jmsUrl = System.getenv("JMS_URL");
-        jmsUser = System.getenv("JMS_USER");
-        jmsPassword = System.getenv("JMS_PASSWORD");
+        String jmsUrl = System.getenv("JMS_URL");
+        String jmsUser = System.getenv("JMS_USER");
+        String jmsPassword = System.getenv("JMS_PASSWORD");
 
         if (jmsUrl == null || jmsUser == null || jmsPassword == null) {
             throw new RuntimeException("Environment Variables JMS_USER, JMS_PASSWORD & JMS_URL must be set, please set and restart the server");
         }
 
         LOG.infov("JMS_URL {0}, JMS_USER {1}, JMS_PASSWORD {2}",jmsUrl, jmsUser, jmsPassword);
-    }
-
-    private ActiveMQConnectionFactory createActiveMQConnectionFactory() {
-//        String jmsUrl = System.getenv("JMS_URL");
-//        String jmsUser = System.getenv("JMS_USER");
-//        String jmsPassword = System.getenv("JMS_PASSWORD");
-//
-//        LOG.info(String.format("URL: %s, User: %s, Password: %s", jmsUrl, jmsUser, jmsPassword));
-
-        // Create a connection factory.
-        final ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(jmsUrl);
-
-        // Pass the username and password.
-        connectionFactory.setUserName(jmsUser);
-        connectionFactory.setPassword(jmsPassword);
-        return connectionFactory;
-    }
-
-    private PooledConnectionFactory createPooledConnectionFactory(ActiveMQConnectionFactory connectionFactory) {
-        // Create a pooled connection factory.
-        final PooledConnectionFactory pooledConnectionFactory = new PooledConnectionFactory();
-        pooledConnectionFactory.setConnectionFactory(connectionFactory);
-        pooledConnectionFactory.setMaxConnections(2);
-        return pooledConnectionFactory;
     }
 
     @Override
