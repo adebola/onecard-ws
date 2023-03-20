6df8f02628d69a8909711c266d6041f066278896
Adebola Omoboya <adeomoboya@gmail.com>
2023-03-20 01:39:14 +0100

    banner.txt added

---
+++ mssc-communication/src/main/java/io/factorialsystems/mssccommunication/controller/ExcelController.java
@@ -0,0 +1,10 @@
+package io.factorialsystems.mssccommunication.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/generate")
+public class ExcelController {
+    
+}
