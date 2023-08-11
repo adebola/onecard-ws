8abd0925e17fe71774d6fdc31af380d6711d2de5
Adebola Omoboya <adeomoboya@gmail.com>
2023-08-11 14:24:19 +0100

    Add new JMS Listener function for asynchronous SMS sending

--- mssc-communication/src/main/java/io/factorialsystems/mssccommunication/service/listener/CommunicationListener.java
+++ mssc-communication/src/main/java/io/factorialsystems/mssccommunication/service/listener/CommunicationListener.java
@@ -3,10 +3,11 @@ package io.factorialsystems.mssccommunication.service.listener;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.factorialsystems.mssccommunication.config.JMSConfig;
 import io.factorialsystems.mssccommunication.dto.MailMessageDto;
+import io.factorialsystems.mssccommunication.dto.SMSMessageDto;
 import io.factorialsystems.mssccommunication.service.mail.MailService;
+import io.factorialsystems.mssccommunication.service.sms.SMSMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
@@ -16,19 +17,26 @@ import java.io.IOException;
 @Service
 @RequiredArgsConstructor
 public class CommunicationListener {
+    private final MailService mailService;
     private final ObjectMapper objectMapper;
-    private final ApplicationContext applicationContext;
+    private final SMSMessageService smsMessageService;
 
     @JmsListener(destination = JMSConfig.SEND_PROVIDER_MAIL_QUEUE)
     public void listenForMailWithoutAttachment(String jsonData) throws IOException {
 
         if (jsonData != null) {
             MailMessageDto mailMessageDto = objectMapper.readValue(jsonData, MailMessageDto.class);
-
             log.info(String.format("Received Asynchronous Request to send Mail %s", mailMessageDto.getTo()));
-
-            MailService mailService = applicationContext.getBean(MailService.class);
             mailService.sendMail(mailMessageDto, null);
+        }
+    }
+
+    @JmsListener(destination = JMSConfig.SEND_SMS_QUEUE)
+    public void listenForSMS(String jsonData) throws IOException {
+        if (jsonData != null) {
+            SMSMessageDto smsMessageDto = objectMapper.readValue(jsonData, SMSMessageDto.class);
+            log.info(String.format("Received Asynchronous Request to send SMS to %s", smsMessageDto.getTo()));
+            smsMessageService.sendMessage(smsMessageDto);
         }
     }
 }
